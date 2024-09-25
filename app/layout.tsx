13320dfd2912f65f29a53dc9a9d084ed2e87ba20
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-09-25 18:32:24 +0200

    fixing root page errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import "./globals.css";
+import type { Metadata } from "next";
+import type { ReactNode } from "react";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Zondiwe&apos;s Funeral Website",
   description: "In memory of Zondiwe",
 };
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>{children}</body>

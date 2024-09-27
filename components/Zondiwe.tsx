75d566ace85b70309063807edb8f423f6ee5f3bf
Jeremiah Chienda <jeremiahchienda@gmail.com>
2024-09-27 11:58:04 +0200

    fixed program errors

--- components/Zondiwe.tsx
+++ components/Zondiwe.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
